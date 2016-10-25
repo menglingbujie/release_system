e7be48a9282babd9e980507a25dcbd487e37af26
menglj <menglingbujie@gmail.com>
2016-10-25 18:15:23 +0800

    修改文案

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Release System' });
+  res.render('index', { title: '在线编译系统' });
 });
 
 module.exports = router;
